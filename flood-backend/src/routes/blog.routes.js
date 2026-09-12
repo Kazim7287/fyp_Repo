19835b40a80c8c7478c26f082b3c7be50d2167b1
Muhammad Kazim Ahmad <23pwcse2233@uetpeshawar.edu.pk>
2026-09-12 14:50:39 +0500

    resolved

--- flood-backend/src/routes/blog.routes.js
+++ flood-backend/src/routes/blog.routes.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 
 const {
@@ -10,12 +9,10 @@ const {
   togglePublish,
   incrementViews,
   getBlogStats,
-  uploadBlogContentImage,
 } = require("../controllers/blog.controller");
 
 const {
   uploadBlogImage,
-  uploadBlogContentImage: uploadContentImage,
 } = require("../middleware/upload.middleware");
 
 const router = express.Router();
@@ -31,21 +28,6 @@ router.get("/", getBlogs);
 // =========================================================
 
 router.get("/stats", getBlogStats);
-
-// =========================================================
-// UPLOAD CONTENT IMAGE
-// =========================================================
-// Used by ReactQuill.
-//
-// This is different from the featured image.
-// Every image inserted into the editor can be uploaded here.
-// =========================================================
-
-router.post(
-  "/upload-image",
-  uploadContentImage.single("contentImage"),
-  uploadBlogContentImage
-);
 
 // =========================================================
 // GET SINGLE BLOG
